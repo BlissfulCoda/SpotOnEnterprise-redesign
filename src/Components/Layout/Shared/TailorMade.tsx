77efd47efed82809710519687667487a837d3833
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2022-11-19 11:57:03 +0000

    fix(client): cleaned up folder structure

--- src/Components/Layout/Shared/TailorMade.tsx
+++ src/Components/Layout/Shared/TailorMade.tsx
@@ -1,7 +1,7 @@
 export default function TailorMade(): JSX.Element {
   return (
     <div
-      className="absolute -rotate-90 -left-[72px] top-[230px] font-serif font-bold w-[800px] opacity-10 text-9xl sm:-left-3 tablet:text-[105px] tablet:left-64 tablet:top-[100px] laptop:left-[510px] laptop:text-[125px] laptop:top-[180px]
+      className="absolute -rotate-90 -left-[72px] top-[230px] font-serif font-bold w-[800px] opacity-5 text-9xl sm:-left-3 tablet:text-[105px] tablet:left-64 tablet:top-[100px] laptop:left-[510px] laptop:text-[125px] laptop:top-[180px]
         desktop:left-[570px] 
         biggest:left-[660px] "
     >
