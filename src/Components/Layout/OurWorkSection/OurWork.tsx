e40860a35015a82e194ba2eb7f0cd8c22bc3c818
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2022-11-21 13:23:48 +0000

    feat(client): Our work section complete

--- src/Components/Layout/OurWorkSection/OurWork.tsx
+++ src/Components/Layout/OurWorkSection/OurWork.tsx
@@ -1,17 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+
+import { DarkModeDataInterface } from "../../../Data/DarkModeData";
+import DarkModeContext from "../../../Context/DarkModeContext";
+
+import { SpotOnEntepriseGifsAndLogos } from "../../../Data/ComponentData";
 
 import { FaAngleLeft } from "react-icons/fa";
-import { motion, useSpring, useTransform } from "framer-motion";
+import {
+  motion,
+  useSpring,
+  useTransform,
+  AnimatePresence,
+} from "framer-motion";
 import { AiOutlineClose } from "react-icons/ai";
 import { ImageList } from "../../../Data/ComponentData";
 
 function OurWork(): JSX.Element {
-  const x = useSpring(0, { stiffness: 900, damping: 150 });
+  const { theme } = useContext(DarkModeContext) as DarkModeDataInterface;
+
+  const { DarkLogo, LightLogo } = SpotOnEntepriseGifsAndLogos;
+
+  // Framer Motion setings
+  const x = useSpring(0, { stiffness: 600, damping: 100 });
   const width = useTransform(x, [-2850, 0], [342, 0]);
   const scale = useTransform(x, [-100, 0], [1.15, 1]);
-  const fadeIn = useTransform(x, [-100, 1], [0.5, 0]);
+  const fadeIn = useTransform(x, [-100, 0], [1, 0]);
+  const fadeOut = useTransform(x, [-60, 0], [0, 1]);
+  const up = useTransform(x, [-100, 0], [-100, 0]);
   // state
   const [state, setState] = useState<boolean>(false);
+
+  // overflow
+  let targetElement = document.querySelector("html");
+  console.log(targetElement)
 
   useEffect(() => {
     // change state when dragged on x - axis is pass -100px
@@ -20,29 +41,61 @@ function OurWork(): JSX.Element {
     });
   }, [x]);
 
+  // No scroll while drag state is active
+  useEffect(() => {
+    state
+      ? targetElement?.classList.add("no-scroll")
+      : targetElement?.classList.remove("no-scroll");
+  });
+
+  const handleCloseDrag = () => {
+    x.stop();
+    x.set(0);
+  };
   return (
     <section
       id="work"
-      className="container px-6 py-12 mx-auto mb-12 space-y-4 duration-1000 tablet:px-0 sm:p-2 laptop:-space-y-4 biggest:max-w-6xl"
+      className="container px-6 py-12 mx-auto mb-20 space-y-4 space-y-6 duration-1000 tablet:px-0 sm:p-2 laptop:-space-y-4 biggest:max-w-6xl"
     >
-      <div className="container mx-auto tablet:px-40 laptop:px-56">
+      <motion.div
+        style={{ translateY: up }}
+        className="container mx-auto duration-100 tablet:px-40 laptop:px-56"
+      >
         <h2>WHAT WE OFFER</h2>
-      </div>
+      </motion.div>
 
       {/* BACKGROUND CONTAINER */}
       <div className="relative">
-        {/* BACKGROUND */}
         <motion.div
-        style={{ opacity: fadeIn }}
-          className="background"
+          style={{ opacity: fadeIn }}
+          className={state ? "background" : "hidden"}
         ></motion.div>
-        <div className="drag-header">
-          <div className="logo">SpotOnEnterprise</div>
-          <div className="close">
-            <AiOutlineClose />
-          </div>
-        </div>
-
+        {/* LOGO AND EXIT */}
+        {state ? (
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0, y: -30 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -30 }}
+              transition={{
+                y: { duration: 0.5 },
+                default: { ease: "linear" },
+              }}
+              className="drag-header"
+            >
+              <img
+                src={theme ? DarkLogo : LightLogo}
+                alt="SpotOnEnterprise Logo"
+                className="w-40 h-12"
+              />
+              <div className="close">
+                <AiOutlineClose onClick={handleCloseDrag} />
+              </div>
+            </motion.div>
+          </AnimatePresence>
+        ) : (
+          <AnimatePresence></AnimatePresence>
+        )}
         {/* IMAGE CONTAINER */}
         <div className="w-[4000px] relative h-[490px] tablet:w-full tablet:h-full ">
           <motion.div
@@ -69,12 +122,13 @@ function OurWork(): JSX.Element {
         </div>
         {/* PROGRESS BAR CONTAINER */}
         <div className="space-y-2 sm:px-3">
-          <div className="flex items-center justify-end mb-4 space-x-1">
+          <motion.div
+            style={{ opacity: fadeOut, x }}
+            className="flex items-center justify-end mb-4 space-x-1"
+          >
             <FaAngleLeft className="text-bgPurple" />
-            <h5 className="font-black text-right opacity-100">
-              Drag To Enlarge
-            </h5>
-          </div>
+            <h5 className="font-black text-right ">Drag To Explore</h5>
+          </motion.div>
 
           {/* PROGESS BAR */}
           <div className="drag-progress-background tablet:hidden ">
